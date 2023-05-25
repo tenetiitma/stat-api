ba618504ab4a2b06264466c686d53ae64ba955cf
Tene Tiitma <tenetiitma@gmail.com>
2023-05-25 11:48:14 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const BASE_URL = "https://andmed.stat.ee/api/v1/et";
+const BASE_URL = "https://visual-crossing-weather.p.rapidapi.com";
+const OPTIONS = {
+    'X-RapidAPI-Key': '7cc19a1227msh8862e54e13083fcp1225f4jsn963fce01679c',
+	'X-RapidAPI-Host': 'visual-crossing-weather.p.rapidapi.com'
+}
 
-const response = await fetch(`${BASE_URL}/stat/majandus/hinnad`);
+const response = await fetch(`${BASE_URL}/forecast?aggregateHours=24&location=Kuressaare,Estonia&contentType=json&unitGroup=metric`, { 
+    headers: OPTIONS
+});
 const data = await response.json();
 
 // Kui console.log annab vastuse "pending" siis on kuskil await puudu.
